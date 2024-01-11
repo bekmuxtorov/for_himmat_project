b303f04b0176885c2a92024dfaf6c4381ca450f1
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-12 01:22:03 +0500

    📝 Added contact default button

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,6 +8,8 @@ from data.config import ADMINS, CHANNELS
 from states.register import Register
 from filters.is_privatechat import IsPrivateChat
 
+from keyboards.default.default_buttons import contact_request_button, make_buttons
+
 
 @dp.message_handler(IsPrivateChat(), CommandStart())
 async def bot_start(message: types.Message):
@@ -34,6 +36,39 @@ async def is_member(call: types.CallbackQuery, state: FSMContext):
     await Register.full_name.set()
 
 
-@dp.message_handler(state=None)
-async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+@dp.message_handler(IsPrivateChat(), state=Register.full_name)
+async def bot_echo(message: types.Message, state: FSMContext):
+    full_name = message.text
+    await state.update_data(full_name=full_name)
+    await message.answer(text="Telefon raqamingizni kiriting:", reply_markup=contact_request_button)
+    await Register.phone_number.set()
+
+
+@dp.message_handler(IsPrivateChat(), state=Register.phone_number, content_types="contact")
+async def register(message: types.Message, state: FSMContext):
+    phone_number = message.contact.phone_number
+    await state.update_data(phone_number=phone_number)
+    await message.answer(text="Jinsingizni tanglang:", reply_markup=make_buttons(["Erkak", "Ayol"], row_width=2))
+    await Register.gender.set()
+
+
+@dp.message_handler(IsPrivateChat(), state=Register.gender)
+async def register(message: types.Message, state: FSMContext):
+    gender = message.text
+    await state.update_data(gender=gender)
+    await message.answer(text="Yoshingizni kiriting:\n\nNamuna: 22")
+    await Register.age.set()
+
+
+@dp.message_handler(IsPrivateChat(), state=Register.age)
+async def register(message: types.Message, state: FSMContext):
+    age = message.text
+    user_data = await state.get_data()
+    user_data["age"] = age
+    user_data["user_id"] = message.from_user.id
+    user_id = message.from_user.id
+    full_name = user_data.get("full_name")
+    phone_number = user_data.get("phone_number")
+    gender = user_data.get("gender")
+
+    print(user_data)
