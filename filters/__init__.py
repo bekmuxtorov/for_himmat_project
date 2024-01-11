4f231d43a1047f6500c0610c0bb947c80b526653
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-12 00:25:30 +0500

    Revert "✏️ Written privatechat filter"
    This reverts commit 2c66c6de755faad20701f9033a3e37b1251512bc.

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,9 @@
 from aiogram import Dispatcher
 
 from loader import dp
-from .is_privatechat import IsPrivateChat
+# from .is_admin import AdminFilter
 
 
 if __name__ == "filters":
-    dp.filters_factory.bind(IsPrivateChat)
-    
+    #dp.filters_factory.bind(is_admin)
+    pass
