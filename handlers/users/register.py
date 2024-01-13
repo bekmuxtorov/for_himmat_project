1244d99d6d5f7233db3ba29cbb93e33b123ebee6
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-13 21:44:04 +0500

    ➕Added register

---
+++ handlers/users/register.py
@@ -0,0 +1,69 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.dispatcher import FSMContext
+
+from loader import dp, db, bot
+from states.register import Register
+from filters.is_privatechat import IsPrivateChat
+
+from keyboards.default.default_buttons import contact_request_button, make_buttons
+from keyboards.inline.buttons import course_buttons
+
+
+@dp.message_handler(IsPrivateChat(), text="Ro'yhatdan o'tish")
+async def register(message: types.Message, state: FSMContext):
+    await message.answer(text="Ism familiyangizni kiriting:\n\nNamuna: Palonchiyev Pistonchi")
+    await Register.full_name.set()
+
+
+@dp.message_handler(IsPrivateChat(), state=Register.full_name)
+async def bot_echo(message: types.Message, state: FSMContext):
+    full_name = message.text
+    await state.update_data(full_name=full_name)
+    await message.answer(text="Telefon raqamingizni kiriting:", reply_markup=contact_request_button)
+    await Register.phone_number.set()
+
+
+@dp.message_handler(IsPrivateChat(), state=Register.phone_number, content_types="contact")
+async def register(message: types.Message, state: FSMContext):
+    phone_number = message.contact.phone_number
+    await state.update_data(phone_number=phone_number)
+    await message.answer(text="Jinsingizni tanglang:", reply_markup=make_buttons(["Erkak", "Ayol"], row_width=2))
+    await Register.gender.set()
+
+
+@dp.message_handler(IsPrivateChat(), state=Register.gender)
+async def register(message: types.Message, state: FSMContext):
+    gender = message.text
+    await state.update_data(gender=gender)
+    await message.answer(text="Yoshingizni kiriting:\n\nNamuna: 22")
+    await Register.age.set()
+
+
+@dp.message_handler(IsPrivateChat(), state=Register.age)
+async def register(message: types.Message, state: FSMContext):
+    age = message.text
+    user_data = await state.get_data()
+    user_data["age"] = age
+    user_data["user_id"] = message.from_user.id
+    user_id = message.from_user.id
+    username = message.from_user.username
+    full_name = user_data.get("full_name")
+    phone_number = user_data.get("phone_number")
+    gender = user_data.get("gender")
+
+    try:
+        await db.add_user(
+            full_name=full_name,
+            username=username,
+            telegram_id=user_id,
+            phone_number=phone_number,
+            gender=gender,
+            age=age
+        )
+        await message.answer(text="Siz muaffaqqiyatli ro'yhatdan o'tdingiz, \n\nQuyidagi yo'nalishlardan birini tanglashingiz mumkin va sizga guruhga qo'shilish uchun link beriladi.", reply_markup=course_buttons)
+        await state.finish()
+    except:
+        await message.answer(text="Ro'yhatdan o'tishda qandaydir muammo chiqdi, iltimos qayta urinib ko'ring!", reply_markup=make_buttons(["Ro'yhatdan o'tish"]))
+        await state.finish()
+        
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,66 +9,27 @@ from states.register import Register
 from filters.is_privatechat import IsPrivateChat
 
 from keyboards.default.default_buttons import contact_request_button, make_buttons
+from keyboards.inline.buttons import course_buttons
 
 
 @dp.message_handler(IsPrivateChat(), CommandStart())
 async def bot_start(message: types.Message):
-    try:
-        user = await db.add_user(telegram_id=message.from_user.id,
-                                 full_name=message.from_user.full_name,
-                                 username=message.from_user.username)
-    except asyncpg.exceptions.UniqueViolationError:
-        user = await db.select_user(telegram_id=message.from_user.id)
 
-    await message.answer("Xush kelibsiz!")
+    user = await db.select_user(telegram_id=message.from_user.id)
+    if user:
+        await message.answer("Marhamat o'zingizga kerakli guruhni tanlang: ", reply_markup=course_buttons)
+    else: 
+        await message.answer("Xush kelibsiz!\n\nBotdan foydalanish uchun quyidagi tugma yordamida ro'yhatdan o'ting!", reply_markup=make_buttons(["Ro'yhatdan o'tish"]))
 
     # ADMINGA xabar beramiz
-    count = await db.count_users()
-    msg = f"{user[1]} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    await bot.send_message(chat_id=ADMINS[0], text=msg)
+    # count = await db.count_users()
+    # msg = f"{user[1]} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+    # await bot.send_message(chat_id=ADMINS[0], text=msg)
 
 
 @dp.callback_query_handler(text_contains="check_button")
-async def is_member(call: types.CallbackQuery, state: FSMContext):
+async def is_member(call: types.CallbackQuery,):
     await call.message.delete()
-    await call.message.answer(text="Barcha kanallarga a'zo bo'ldingiz! \n\nIsm familiyangizni kiriting!")
+    await call.message.answer(text="Barcha kanallarga a'zo bo'ldingiz! \n\nRo'yatdan o'tish uchun quyidagi tugmani bosing.", reply_markup=make_buttons(["Ro'yhatdan o'tish"]))
     await call.answer(cache_time=60)
-    await Register.full_name.set()
 
-
-@dp.message_handler(IsPrivateChat(), state=Register.full_name)
-async def bot_echo(message: types.Message, state: FSMContext):
-    full_name = message.text
-    await state.update_data(full_name=full_name)
-    await message.answer(text="Telefon raqamingizni kiriting:", reply_markup=contact_request_button)
-    await Register.phone_number.set()
-
-
-@dp.message_handler(IsPrivateChat(), state=Register.phone_number, content_types="contact")
-async def register(message: types.Message, state: FSMContext):
-    phone_number = message.contact.phone_number
-    await state.update_data(phone_number=phone_number)
-    await message.answer(text="Jinsingizni tanglang:", reply_markup=make_buttons(["Erkak", "Ayol"], row_width=2))
-    await Register.gender.set()
-
-
-@dp.message_handler(IsPrivateChat(), state=Register.gender)
-async def register(message: types.Message, state: FSMContext):
-    gender = message.text
-    await state.update_data(gender=gender)
-    await message.answer(text="Yoshingizni kiriting:\n\nNamuna: 22")
-    await Register.age.set()
-
-
-@dp.message_handler(IsPrivateChat(), state=Register.age)
-async def register(message: types.Message, state: FSMContext):
-    age = message.text
-    user_data = await state.get_data()
-    user_data["age"] = age
-    user_data["user_id"] = message.from_user.id
-    user_id = message.from_user.id
-    full_name = user_data.get("full_name")
-    phone_number = user_data.get("phone_number")
-    gender = user_data.get("gender")
-
-    print(user_data)
