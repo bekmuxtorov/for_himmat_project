9fe0a5ffd3379366df63b9584c87cf839f1b6f8f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-16 00:19:19 +0500

    ➕ Added lessons_links.py

---
+++ handlers/users/lesson_links.py
@@ -0,0 +1,12 @@
+from aiogram import types
+from loader import dp, db
+
+from filters.is_privatechat import IsPrivateChat
+from keyboards.inline.buttons import course_button
+
+
+@dp.message_handler(IsPrivateChat(), text="Dars uchun link olish")
+async def bot_echo(message: types.Message):
+    user = await db.select_user(telegram_id=message.from_user.id)
+    gender = user.get("gender")
+    await message.answer("Quyidagi tugmalardan birini tanglashingiz va guruhga qo'shilishingiz mumkin.", reply_markup=course_button(gender))
