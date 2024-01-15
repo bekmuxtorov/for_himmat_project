fb0241255fad95f451f40daedaac38a4296c42dd
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-16 00:11:08 +0500

    ➕ Added some function

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -4,4 +4,8 @@ from . import admin
 from . import cancel
 from . import register
 from . import send_question
+from . import suggests
+from . import social_links
+from . import lesson_links
+from . import conversation
 from . import echo
