fca29bd4b43da06306eed4bd8d63de7a2e638fb1
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-01-16 00:11:51 +0500

    ✏️ Update send_text and added cash_time in call

--- handlers/users/send_question.py
+++ handlers/users/send_question.py
@@ -31,14 +31,16 @@ async def send_question(call: types.CallbackQuery, state: FSMContext):
     question_data = await state.get_data()
     question_text = question_data.get("question_text")
     if username:
-        send_text = f"<b>Kimdan:</b> {full_name}\n<b>Telegam username:</b> @{username}\n\n<i>{question_text}</i>"
+        send_text = f"📝 Ustozga savol yo'llandi.\n\n<b>Kimdan:</b> {full_name}\n<b>Telegam username:</b> @{username}\n\n<i>{question_text}</i>"
     for admin in ADMINS:
         await bot.send_message(chat_id=admin, text=send_text)
     await call.message.answer("✅ Savol muvaffaqiyatli yuborildi!", reply_markup=make_buttons(["Suhbatlar", "Ustozga savol yo'llash", "Dars uchun link olish", "Taklif va e'tirozlar", "Ijtimoiy tarmoq havolalar"], row_width=1))
+    await call.answer(cache_time=60)
     await state.finish()
 
 
 @dp.callback_query_handler(text_contains="no_send", state=SendQuestionToTeacher.confirmation)
 async def send_question(call: types.CallbackQuery, state: FSMContext):
     await call.message.answer("📝 Savol bekor qilindi!", reply_markup=make_buttons(["Suhbatlar", "Ustozga savol yo'llash", "Dars uchun link olish", "Taklif va e'tirozlar", "Ijtimoiy tarmoq havolalar"], row_width=1))
+    await call.answer(cache_time=60)
     await state.finish()
