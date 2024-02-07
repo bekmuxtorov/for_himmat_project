571f56c870f2f25c84e9c33931f4af8cafb37c86
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-07 23:42:23 +0500

    ➕ Added filter

--- handlers/users/home_page.py
+++ handlers/users/home_page.py
@@ -2,8 +2,8 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from loader import dp, db
-from filters.is_privatechat import IsPrivateChatForCallback
-from keyboards.default.default_buttons import make_buttons, build_menu_buttons
+from filters.is_privatechat import IsPrivateChatForCallback, IsPrivateChat
+from keyboards.default.default_buttons import make_buttons, build_menu_buttons, not_registered_for_menu_buttons
 
 
 # Echo bot
@@ -19,12 +19,12 @@ async def bot_echo(call: types.CallbackQuery, state: FSMContext = '*'):
     await state.reset_data()
 
 
-@dp.message_handler(text="🏠 Bosh sahifa", state='*')
+@dp.message_handler(IsPrivateChat(), text="🏠 Bosh sahifa", state='*')
 async def bot_echo(message: types.Message, state: FSMContext = '*'):
     user_id = message.from_user.id
     user = await db.select_user(telegram_id=user_id)
     if not user:
-        await message.answer("Xush kelibsiz!\n\nBotdan foydalanish uchun quyidagi tugma yordamida ro'yhatdan o'ting!", reply_markup=make_buttons(["Ro'yhatdan o'tish"]))
+        await message.answer("✅ Xush kelibsiz!\n\n", reply_markup=not_registered_for_menu_buttons)
     else:
         await message.answer("🏠 Bosh sahifa", reply_markup=build_menu_buttons)
     await state.finish()
