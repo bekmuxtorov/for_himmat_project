9c3b5bdd0a4fd315c33fc02fd5ee05c60f936540
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-07 23:51:42 +0500

    📝 Updated echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,7 +2,7 @@ from aiogram import types
 from loader import dp, db
 
 from filters.is_privatechat import IsPrivateChat
-from keyboards.default.default_buttons import make_buttons, build_menu_buttons
+from keyboards.default.default_buttons import make_buttons, not_registered_for_menu_buttons
 
 # Echo bot
 
@@ -12,4 +12,4 @@ async def bot_echo(message: types.Message):
     if await db.select_user(telegram_id=message.from_user.id):
         await message.answer("🛠️ Aniqlanmagan buyruq!")
     else:
-        await message.answer("Xush kelibsiz!\n\nBotdan foydalanish uchun quyidagi tugma yordamida ro'yhatdan o'ting!", reply_markup=make_buttons(["Ro'yhatdan o'tish"]))
+        await message.answer("✅ Xush kelibsiz!\n\n", reply_markup=not_registered_for_menu_buttons)
