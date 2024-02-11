b6ee0d3cd756fc785636ab54163493d4bd5fbe76
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-11 21:02:25 +0500

    📝 Check chattype

--- handlers/users/cancel.py
+++ handlers/users/cancel.py
@@ -11,13 +11,16 @@ from states.register import Register
 async def bot_echo(message: types.Message, state: FSMContext = '*'):
     user_id = message.from_user.id
     user = await db.select_user(telegram_id=user_id)
-    if not user:
-        state_name = await state.get_state()
-        if state_name in Register.all_states_names:
-            await message.answer("❌ Jarayon bekor qilindi.\n\nQayta urinish uchun quyidagi tugmani ezing!", reply_markup=make_buttons(["Ro'yhatdan o'tish"]))
+    if message.chat.type == types.ChatType.PRIVATE:
+        if not user:
+            state_name = await state.get_state()
+            if state_name in Register.all_states_names:
+                await message.answer("❌ Jarayon bekor qilindi.\n\nQayta urinish uchun quyidagi tugmani ezing!", reply_markup=make_buttons(["Ro'yhatdan o'tish"]))
+            else:
+                await message.answer("❌ Jarayon bekor qilindi.", reply_markup=not_registered_for_menu_buttons)
         else:
-            await message.answer("❌ Jarayon bekor qilindi.", reply_markup=not_registered_for_menu_buttons)
-    else:
-        await message.answer("❌ Jarayon bekor qilindi.", reply_markup=build_menu_buttons)
+            await message.answer("❌ Jarayon bekor qilindi.", reply_markup=build_menu_buttons)
+        
+        
     await state.finish()
     await state.reset_data()
